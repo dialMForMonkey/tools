f27cc9a2385a47f825c4388d71c95ac3776682ab
Gabriel Souza <gabrielsouza849@gmail.com>
2020-05-24 19:10:13 -0300

    algumas unidades de testes

--- commanders/benckmark_memcached_redis/src/memcached.rs
+++ commanders/benckmark_memcached_redis/src/memcached.rs
@@ -1,19 +1,59 @@
+#[cfg(test)]
+mod test {
+    use crate::memcached::BenckmarkMemcached;
+    use crate::traits::BasicFunctions;
+    use std::time::Duration;
 
-use super::traits::basic_functions;
+    #[test]
+    fn conn_ok() {
+        BenckmarkMemcached::new(&"memcache://127.0.0.1:11211");
+    }
+
+    #[test]
+    fn set_ok() {
+        let mut conn = BenckmarkMemcached::new(&"memcache://127.0.0.1:11211");
+        let r = conn.set(&"mario", &"aquele q ....", None);
+        assert!(r.is_ok())
+    }
+
+    #[test]
+    fn set_nok() {
+        let mut conn = BenckmarkMemcached::new(&"memcache://127.0.0.1:11211");
+        //simular queda no servico
+        println!("pausando ...");
+        std::thread::sleep(Duration::from_secs(10));
+        println!("... retornando");
+        let r = conn.set(&"mario", &"aquele q ....", None);
+        assert!(r.is_err())
+    }
+}
+
+use super::traits::BasicFunctions;
 use memcache;
+use memcache::Client;
 
-struct BenckmarkMemcached;
+struct BenckmarkMemcached {
+    conn: Client,
+}
 
-impl basic_functions for BenckmarkMemcached {
+impl BasicFunctions for BenckmarkMemcached {
     fn new(host: &str) -> Self {
-        unimplemented!()
+        let instancy_memcache = BenckmarkMemcached {
+            conn: memcache::Client::connect(host).expect("Erro ao recuperar conexao do memcached"),
+        };
+
+        instancy_memcache
     }
 
-    fn set(self, key: &str, value: &str) -> Self {
-        unimplemented!()
+    fn set(&mut self, key: &str, value: &str, expiration: Option<u32>) -> Result<(), String> {
+        let result = self.conn.set(key, value, expiration.unwrap_or(0u32));
+        match result {
+            Ok(_) => Ok(()),
+            Err(err) => Err(format!("Erro ao setar valor {:?}", err)),
+        }
     }
 
-    fn get(self, key: &str) -> Self {
+    fn get(&mut self, key: &str) {
         unimplemented!()
     }
 }
--- commanders/benckmark_memcached_redis/src/traits.rs
+++ commanders/benckmark_memcached_redis/src/traits.rs
@@ -1,7 +1,6 @@
-
-
-pub trait basic_functions {
+//implementar composer no get e set
+pub trait BasicFunctions {
     fn new(host: &str) -> Self;
-    fn set(self, key: &str, value: &str) -> Self;
-    fn get(self, key: &str) -> Self;
+    fn set(&mut self, key: &str, value: &str, expiration: Option<u32>) -> Result<(), String>;
+    fn get(&mut self, key: &str);
 }
